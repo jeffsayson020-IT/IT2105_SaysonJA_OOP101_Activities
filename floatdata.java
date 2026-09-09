2a4f5cfb5f6fa75cb3691189a4f85306b5270625
Jeff <jeffsayson020@gmail.com>
2026-09-09 22:01:09 +0800

    Add files via upload

---
+++ floatdata.java
@@ -0,0 +1,10 @@
+class FloatData{
+    public static void main(String[]args){
+        double num1 = 17.4;
+        float num2 = 93.5f;
+        int num3 = 103;
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        System.out.println("Nu,ber 3: " + num3);
+    }
+}
